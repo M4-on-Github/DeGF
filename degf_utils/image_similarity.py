643b290d224d1726b6e05fe235ca0c5973d5016d
Ce Zhang <cezhang@cs.cmu.edu>
2024-12-03 13:12:56 -0800

    initial upload

---
+++ degf_utils/image_similarity.py
@@ -0,0 +1,25 @@
+import torch
+from PIL import Image
+from transformers import AutoProcessor, CLIPModel
+import torch.nn as nn
+
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
+model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
+
+def get_clip_similarity(image1, image2):
+    #Extract features from image1
+    with torch.no_grad():
+        inputs1 = processor(images=image1, return_tensors="pt").to(device)
+        image_features1 = model.get_image_features(**inputs1)
+
+    #Extract features from image2
+    with torch.no_grad():
+        inputs2 = processor(images=image2, return_tensors="pt").to(device)
+        image_features2 = model.get_image_features(**inputs2)
+
+    #Compute their cosine similarity and convert it into a score between 0 and 1
+    cos = nn.CosineSimilarity(dim=0)
+    sim = cos(image_features1[0],image_features2[0]).item()
+
+    return sim
---
+++ eval_bench/scripts/pope_eval.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+seed=42
+dataset_name="coco" # coco | aokvqa | gqa
+type="random" # random | popular | adversarial
+
+# llava
+model="llava"
+model_path="/data/ce/model/llava-v1.5-7b"
+
+# instructblip
+# model="instructblip"
+# model_path=None
+
+pope_path="/data/ce/data/POPE/${dataset_name}/${dataset_name}_pope_${type}.json"
+data_path="/data/ce/data/coco/val2014"
+
+# data_path="/data/ce/data/gqa/images"
+
+log_path="./logs"
+
+use_ritual=True
+use_vcd=False
+use_m3id=False
+use_diffusion=False
+
+degf_alpha_pos=3.0
+degf_alpha_neg=1.0
+degf_beta=0.25
+
+experiment_index=3
+
+#####################################
+# Run single experiment
+#####################################
+export CUDA_VISIBLE_DEVICES=2
+torchrun --nnodes=1 --nproc_per_node=1 --master_port 1236 eval_bench/pope_eval_${model}.py \
+--seed ${seed} \
+--model_path ${model_path} \
+--model_base ${model} \
+--pope_path ${pope_path} \
+--data_path ${data_path} \
+--log_path ${log_path} \
+--use_ritual ${use_ritual} \
+--use_vcd ${use_vcd} \
+--use_m3id ${use_m3id} \
+--use_diffusion ${use_diffusion} \
+--degf_alpha_pos ${degf_alpha_pos} \
+--degf_alpha_neg ${degf_alpha_neg} \
+--degf_beta ${degf_beta} \
+--experiment_index ${experiment_index}
